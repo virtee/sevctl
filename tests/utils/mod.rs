c8f9728a65a24d10ea8c7cfab110497ca7d2fc4a
Cole Robinson <crobinso@redhat.com>
2022-05-18 10:14:45 -0400

    tests: Add `vmsa build` tests
    This performs two `vmsa build` tests and compares against known
    good output saved in `tests/data`.
    
    Signed-off-by: Cole Robinson <crobinso@redhat.com>

--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,5 +1,18 @@
 pub const SEVCTL: &'static str = env!("CARGO_BIN_EXE_sevctl");
 
+pub fn cargo_root_path(base: &str) -> String {
+    /*! Take a path relative to the project root, like `tests/data/FOO`,
+    ! and make it absolute !*/
+    format!("{}/{}", env!("CARGO_MANIFEST_DIR"), base)
+}
+
+pub fn compare_files(actual: &str, expected: &str) {
+    let expected = cargo_root_path(expected);
+    let data1 = std::fs::read(actual).unwrap();
+    let data2 = std::fs::read(&expected[..]).unwrap();
+    assert_eq!(data1, data2);
+}
+
 pub fn run_sevctl(arglist: &[&str]) -> String {
     let output = std::process::Command::new(SEVCTL)
         .args(arglist)
